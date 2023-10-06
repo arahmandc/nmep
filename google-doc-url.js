a651df2220068aae7c08de2c873a518760a609d9
Atikur Rahman <55056373+arahmandc@users.noreply.github.com>
2023-10-06 20:37:42 +0600

    file uploaded

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/12mC3TziigEVopm6J9q5ytmlSmwKiQSe6POjojepAkq4/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
